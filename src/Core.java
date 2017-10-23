679c84f864e8c62f5ae01257ddfe5cfd2381296a
s-linben <s-linben@bsd405.org>
2017-10-23 14:49:29 -0700

    For Branch Method Test Import

--- src/Core.java
+++ src/Core.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 
 public class Core {
 	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		
+		
+		
 		System.out.println("Test");
 		//Testing Testing
+		
+		console.close();
 	}
 }
