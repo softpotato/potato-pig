def4ff2e2a25840498d86843aacc8b777a7d9bfe
Softpotato <Softp@DESKTOP-OQROHPJ>
2018-06-22 17:48:15 -0700

    Basic Classes Initialized
    IDK how this game is going to go. Personal project.

---
+++ src/core/Scoreboard.java
@@ -0,0 +1,6 @@
+package core;
+
+public class Scoreboard {
+	protected int value;
+	
+}
---
+++ src/core/Visual.java
@@ -0,0 +1,18 @@
+package core;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+public class Visual extends Application {
+	
+	@Override
+	public void start(Stage primarystage) {
+		
+		
+		
+	}
+	
+}
